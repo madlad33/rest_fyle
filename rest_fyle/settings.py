9649a9c26eb6429cd31bc69f01efd03c71807a44
Tanmay <tanmay.milky33@gmail.com>
2021-01-23 12:48:17 +0530

    Update settings.py

--- rest_fyle/settings.py
+++ rest_fyle/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
         'PORT': '5433',
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
